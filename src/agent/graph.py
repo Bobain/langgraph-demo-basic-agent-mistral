92a6ce64a417d3a0cbd8b1d3cbfe9c11e30494f4
bobain <romain.burgot@gmail.com>
2025-11-07 08:42:17 +0100

    adding a conditional edge to loop if no criteria met, or run queries if some of them are

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -71,7 +71,7 @@ class State:
     message_count: int = 0
 
 
-def model_introduction(state: State):
+async def model_introduction(state: State):
     return {
         "last_user_message": state.last_user_message,
         "message_count": state.message_count + 1,
@@ -81,7 +81,17 @@ def model_introduction(state: State):
     }
 
 
-async def call_model(state: State) -> Dict[str, Any]:
+async def look_for_travel(state: State):
+    return {
+        "last_user_message": state.last_user_message,
+        "message_count": state.message_count + 1,
+        "ai_structured_output": "Let me have a look",
+        "last_ai_message": "Let me have a look",
+        # f"Configured with {runtime.context.get('my_configurable_param')}"
+    }
+
+
+async def criteria_extractor_model(state: State) -> Dict[str, Any]:
     """Process input and returns output.
 
     , runtime: Runtime[Context]
@@ -94,22 +104,27 @@ async def call_model(state: State) -> Dict[str, Any]:
     chat_model = chat_model.with_structured_output(Criteres)
 
     res = await chat_model.ainvoke(state.last_user_message)
-    return {
-        "last_user_message": state.last_user_message,
-        "message_count": state.message_count + 1,
-        "ai_structured_output": res,
-        "last_ai_message": ai_role_message,
-        # f"Configured with {runtime.context.get('my_configurable_param')}"
-    }
+    if any(v is not None for v in res.model_dump().values()):
+        return look_for_travel.__name__
+    else:
+        return model_introduction.__name__
+    # return {
+    #     "last_user_message": state.last_user_message,
+    #     "message_count": state.message_count + 1,
+    #     "ai_structured_output": res,
+    #     "last_ai_message": state.last_ai_message,
+    #     # f"Configured with {runtime.context.get('my_configurable_param')}"
+    # }
 
 
 # Define the graph
 builder = StateGraph(State, context_schema=Context)
 builder.add_node(model_introduction.__name__, model_introduction)
-builder.add_node(call_model.__name__, call_model)
+builder.add_node(criteria_extractor_model.__name__, criteria_extractor_model)
+builder.add_node(look_for_travel.__name__, look_for_travel)
 
-builder.add_edge("__start__", "call_model")
-builder.add_conditional_edges("model_introduction", "call_model")
+builder.add_edge("__start__", model_introduction.__name__)
+builder.add_conditional_edges(model_introduction.__name__, criteria_extractor_model)
 
 graph = builder.compile(name="New Graph")
 
