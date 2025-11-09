3fddd21a034005c4c95057e4bf168db997a249c7
bobain <romain.burgot@gmail.com>
2025-11-09 18:36:04 +0100

    more prints, but still a strange graph viz

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -12,7 +12,7 @@ from typing import Any, Dict
 from typing_extensions import TypedDict
 
 from langchain.chat_models import init_chat_model
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
 import langsmith as ls  # noqa: F401
 from pydantic import BaseModel
 from langchain_tavily import TavilySearch
@@ -21,41 +21,8 @@ MAX_TAVILY_RESULTS = 5
 TAVILY_INJECT_RAW_USER_MEASSAGE = False
 
 
-async def search_travel(state: State):
-    """Search for general web results.
-
-    This function performs a search using the Tavily search engine, which is designed
-    to provide comprehensive, accurate, and trusted results. It's particularly useful
-    for answering questions about current events.
-    """
-    wrapped = TavilySearch(max_results=MAX_TAVILY_RESULTS)
-    criteria = state.ai_structured_output
-    query = "Trouve un voyage tout organisé qui respecte les critères suivants \n: "
-    for criterion, value in criteria.model_dump().items():
-        if value is not None and not criterion == "acces_handicap":
-            query += f"{criterion} = {value} "
-
-    if criteria.acces_handicap:
-        query += (
-            "\n ATTENTION cette personne est handicapée, "
-            "il faut absolument que tu trouves un voyage qui respecte les critères mais soit accessible à une personne handicapée"
-        )
-    res = await wrapped.ainvoke({"query": query})
-
-    results = []
-    for result in res["results"]:
-        results.append(result["title"] + " : " + result["url"])
-
-    return {
-        "last_user_message": state.last_user_message,
-        "message_count": state.message_count + 1,
-        "ai_structured_output": state.ai_structured_output,
-        "last_ai_message": "\n".join(results),
-    }
-
-
 ## This Sate allows additional fields to be added on top of the of ones already defined,
-#   therefore : it would allow any criterie "mentionned" by the user
+#   therefore : it would allow any criterie "mentionned" by the user but not in the list
 # @dataclass
 # class Criteres:
 #     criteres: dict[str, bool | None] = field(
@@ -149,21 +116,54 @@ async def criteria_extractor_model(state: State) -> Dict[str, Any]:
     }
 
 
+async def search_travel(state: State):
+    """Search for general web results.
+
+    This function performs a search using the Tavily search engine, which is designed
+    to provide comprehensive, accurate, and trusted results. It's particularly useful
+    for answering questions about current events.
+    """
+    wrapped = TavilySearch(max_results=MAX_TAVILY_RESULTS)
+    criteria = state.ai_structured_output
+    query = "Trouve un voyage tout organisé qui respecte les critères suivants \n: "
+    for criterion, value in criteria.model_dump().items():
+        if value is not None and not criterion == "acces_handicap":
+            query += f"{criterion} = {value} "
+
+    if criteria.acces_handicap:
+        query += (
+            "\n ATTENTION cette personne est handicapée, "
+            "il faut absolument que tu trouves un voyage qui respecte les critères mais soit accessible à une personne handicapée"
+        )
+    res = await wrapped.ainvoke({"query": query})
+
+    results = []
+    for result in res["results"]:
+        results.append(result["title"] + " : " + result["url"])
+
+    print(results)
+
+    return {
+        "last_user_message": state.last_user_message,
+        "message_count": state.message_count + 1,
+        "ai_structured_output": state.ai_structured_output,
+        "last_ai_message": "\n".join(results),
+    }
+
+
 # Define the graph
 builder = StateGraph(State, context_schema=Context)
-builder.add_node(criteria_no_match.__name__, criteria_no_match)
 builder.add_node(criteria_extractor_model.__name__, criteria_extractor_model)
+builder.add_node(criteria_no_match.__name__, criteria_no_match)
 builder.add_node(search_travel.__name__, search_travel)
 
-# builder.add_edge("__start__", criteria_extractor_model.__name__)
-
-builder.add_edge("__start__", criteria_extractor_model.__name__)
+builder.add_edge(START, criteria_extractor_model.__name__)
 
 builder.add_conditional_edges(criteria_extractor_model.__name__, criteria_router)
-builder.add_edge(search_travel.__name__, "__end__")
-builder.add_edge(criteria_no_match.__name__, "__end__")
+builder.add_edge(search_travel.__name__, END)
+builder.add_edge(criteria_no_match.__name__, END)
 
-graph = builder.compile(name="New Graph")
+graph = builder.compile(name="Travel searcher")
 
 
 if __name__ == "__main__":
@@ -171,10 +171,14 @@ if __name__ == "__main__":
     from dotenv import load_dotenv
 
     load_dotenv()
+
+    print(graph.get_graph().draw_ascii())
+
     # state = State(last_user_message="J'aime le sport et la randonnée dans le désert")
     state = State(last_user_message="J'aime le sport et la randonnée, mais je suis une personne à mobilité réduite")
     state = State(last_user_message="J'aime la randonnée, mais je suis PMR")
     state = State(last_user_message="J'aime la randonnée à kla campagne, mais je suis PMR")
+    state = State(last_user_message="J'aime les balades à à dos de dromadaire. Je suis PMR")
     # state = State(last_user_message="Bonjour")
     # state = State(last_user_message="J'aime le sport et la et la randonnée")
     print(asyncio.run(graph.ainvoke(state)))
