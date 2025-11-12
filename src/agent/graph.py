ec0afc0628b191470390eb041141860cf03b5666
bobain <romain.burgot@gmail.com>
2025-11-12 16:52:22 +0100

    retour au cahier des charges

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -4,8 +4,9 @@ Returns a predefined response. Replace logic and configuration as needed.
 """
 
 from __future__ import annotations
-from typing import Literal
 from dataclasses import dataclass
+from pydantic import BaseModel
+import unidecode
 
 # To support Python < 3.12 which is used in LangGraph Docker image with langgraph up
 from typing_extensions import TypedDict
@@ -13,30 +14,32 @@ from typing_extensions import TypedDict
 from langchain.chat_models import init_chat_model
 from langgraph.graph import StateGraph, START, END
 import langsmith as ls  # noqa: F401
-from pydantic import BaseModel
-from langchain_tavily import TavilySearch
 
-MAX_TAVILY_RESULTS = 5
-TAVILY_INJECT_RAW_USER_MEASSAGE = False
+OUTPUT_TRAVELS = [
+    {"nom": "Randonnée camping en Lozère", "labels": ["sport", "montagne", "campagne"], "accessibleHandicap": "non"},
+    {
+        "nom": "5 étoiles à Chamonix option fondue",
+        "labels": ["montagne", "détente"],
+        "accessibleHandicap": "oui",
+    },
+    {
+        "nom": "5 étoiles à Chamonix option ski",
+        "labels": ["montagne", "sport"],
+        "accessibleHandicap": "non",
+    },
+    {
+        "nom": "Palavas de paillotes en paillotes",
+        "labels": ["plage", "ville", "détente", "paillote"],
+        "accessibleHandicap": "oui",
+    },
+    {
+        "nom": "5 étoiles en rase campagne",
+        "labels": ["campagne", "détente"],
+        "accessibleHandicap": "oui",
+    },
+]
 
 
-## This Sate allows additional fields to be added on top of the of ones already defined,
-#   therefore : it would allow any criterie "mentionned" by the user but not in the list
-# @dataclass
-# class Criteres:
-#     criteres: dict[str, bool | None] = field(
-#         default_factory=lambda: {
-#             "plage": None,
-#             "montagne": None,
-#             "ville": None,
-#             "sport": None,
-#             "detente": None,
-#             "acces_handicap": None,
-#         }
-#     )
-
-
-# This version is more strict and will ignore extra fields
 class Criteres(BaseModel):
     plage: bool | None = None
     montagne: bool | None = None
@@ -46,10 +49,38 @@ class Criteres(BaseModel):
     acces_handicap: bool | None = None
 
 
-ai_role_message = f"""Bonjour, je suis un assistant qui va vous aider à planifier votre prochain voyage.
-        Vous pouvez me parler naturellement, mais sachez que je vais limiter mes recherches de voyages aux critères suivants:
-        {', '.join(list(Criteres.model_fields.keys()))}
-        """
+def match_criteria_and_travels(criteres: Criteres):
+    if criteres.acces_handicap:
+        output_travels = []
+        for travels in OUTPUT_TRAVELS:
+            if travels["accessibleHandicap"] == "oui":
+                output_travels.append(travels)
+    else:
+        output_travels = OUTPUT_TRAVELS
+
+    scores_for_travels = [0] * len(output_travels)
+    for num_travel, travels in enumerate(output_travels):
+        labels = [unidecode.unidecode(label) for label in travels["labels"]]
+        for criterion, criterion_yes_no in criteres.model_dump().items():
+            if not criterion_yes_no and criterion in labels:
+                scores_for_travels[num_travel] += -1
+            elif criterion_yes_no and criterion in labels:
+                scores_for_travels[num_travel] += 1
+            elif criterion_yes_no and criterion not in labels:
+                scores_for_travels[num_travel] += -1
+
+    max_score = -100
+    num_best_travel = None
+    for i, score in enumerate(scores_for_travels):
+        if score > max_score:
+            max_score = score
+            num_best_travel = i
+
+    return output_travels[num_best_travel]
+
+
+class MessageUnderstandable(BaseModel):
+    answer: bool | None = None
 
 
 class Context(TypedDict):
@@ -76,24 +107,7 @@ class State:
     message_count: int = 0
 
 
-async def criteria_no_match(state: State):
-    return {
-        "last_user_message": "",
-        "message_count": state.message_count + 1,
-        "ai_structured_output": None,
-        "last_ai_message": ai_role_message,
-        # f"Configured with {runtime.context.get('my_configurable_param')}"
-    }
-
-
-async def criteria_router(state: State) -> Literal["search_travel", "criteria_no_match"]:
-    if any(v is not None for v in state.ai_structured_output.model_dump().values()):
-        return "search_travel"
-    else:
-        return "criteria_no_match"
-
-
-async def criteria_extractor_model(state: State) -> State:
+async def chat_model(state: State):
     """Process input and returns output.
 
     , runtime: Runtime[Context]
@@ -103,66 +117,64 @@ async def criteria_extractor_model(state: State) -> State:
     # see https://docs.mistral.ai/getting-started/models/models_overview/
 
     chat_model = init_chat_model(model="codestral-2508", model_provider="mistralai")
-    chat_model = chat_model.with_structured_output(Criteres)
+    res = chat_model.with_structured_output(MessageUnderstandable).invoke(
+        f"Le message suivant est-il compréhensible? \n {state.last_user_message}"
+    )
 
-    res = await chat_model.ainvoke(state.last_user_message)
-    return {
-        "last_user_message": state.last_user_message,
-        "message_count": state.message_count + 1,
-        "ai_structured_output": res,
-        "last_ai_message": state.last_ai_message,
-        # f"Configured with {runtime.context.get('my_configurable_param')}"
-    }
+    if not res.answer:
+        return {
+            "last_user_message": state.last_user_message,
+            "message_count": state.message_count + 1,
+            "ai_structured_output": state.ai_structured_output,
+            "last_ai_message": "Désolé je n'ai pas compris votre message",
+            # f"Configured with {runtime.context.get('my_configurable_param')}"
+        }
 
+    criteres = chat_model.with_structured_output(Criteres).invoke(state.last_user_message)
+    print(criteres)
+    if all(v is None for v in res.model_dump().values()):
+        return {
+            "last_user_message": state.last_user_message,
+            "message_count": state.message_count + 1,
+            "ai_structured_output": None,
+            "last_ai_message": f"""Bonjour, je suis un assistant qui va vous aider à planifier votre prochain voyage.
+                Vous pouvez me parler naturellement, mais sachez que je vais limiter mes recherches de voyages aux critères suivants:
+                {', '.join(list(Criteres.model_fields.keys()))}
+                """,
+            # f"Configured with {runtime.context.get('my_configurable_param')}"
+        }
 
-async def search_travel(state: State):
-    """Search for general web results.
-
-    This function performs a search using the Tavily search engine, which is designed
-    to provide comprehensive, accurate, and trusted results. It's particularly useful
-    for answering questions about current events.
-    """
-    wrapped = TavilySearch(max_results=MAX_TAVILY_RESULTS)
-    criteria = state.ai_structured_output
-    query = "Trouve un voyage tout organisé qui respecte les critères suivants \n: "
-    for criterion, value in criteria.model_dump().items():
-        if value is not None and not criterion == "acces_handicap":
-            query += f"{criterion} = {value} "
-
-    if criteria.acces_handicap:
-        query += (
-            "\n ATTENTION cette personne est handicapée, "
-            "il faut absolument que tu trouves un voyage qui respecte les critères mais soit accessible à une personne handicapée"
-        )
-    res = await wrapped.ainvoke({"query": query})
-
-    results = []
-    for result in res["results"]:
-        results.append(result["title"] + " : " + result["url"])
-
-    print(results)
+    if state.ai_structured_output is None:
+        # Initiating Criteria, we set them to None as asked, when not mentionned
+        for key, val in criteres.model_dump().items():
+            if val is None:
+                setattr(criteres, key, False)
+        state.ai_structured_output = criteres
+    else:
+        # Updating Criteria
+        #   we don't set to False a criterion which was previously mentioned but is not in the last message
+        for key, val in criteres.model_dump().items():
+            if val is not None:
+                setattr(state.ai_structured_output, key, val)
 
     return {
         "last_user_message": state.last_user_message,
         "message_count": state.message_count + 1,
         "ai_structured_output": state.ai_structured_output,
-        "last_ai_message": "\n".join(results),
+        "last_ai_message": match_criteria_and_travels(state.ai_structured_output),
+        # f"Configured with {runtime.context.get('my_configurable_param')}"
     }
 
 
 # Define the graph
 builder = StateGraph(State, context_schema=Context)
-builder.add_node(criteria_extractor_model.__name__, criteria_extractor_model)
-builder.add_node(criteria_no_match.__name__, criteria_no_match)
-builder.add_node(search_travel.__name__, search_travel)
 
-builder.add_edge(START, criteria_extractor_model.__name__)
+builder.add_node(chat_model.__name__, chat_model)
 
-builder.add_conditional_edges(criteria_extractor_model.__name__, criteria_router)
-builder.add_edge(search_travel.__name__, END)
-builder.add_edge(criteria_no_match.__name__, END)
+builder.add_edge(START, chat_model.__name__)
+builder.add_edge(chat_model.__name__, END)
 
-graph = builder.compile(name="Travel searcher", checkpointer=None)
+graph = builder.compile(name="Travel searcher")
 
 
 if __name__ == "__main__":
@@ -171,13 +183,5 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    print(graph.get_graph().draw_ascii())
-
-    # state = State(last_user_message="J'aime le sport et la randonnée dans le désert")
-    state = State(last_user_message="J'aime le sport et la randonnée, mais je suis une personne à mobilité réduite")
-    state = State(last_user_message="J'aime la randonnée, mais je suis PMR")
-    state = State(last_user_message="J'aime la randonnée à kla campagne, mais je suis PMR")
-    state = State(last_user_message="J'aime les balades à à dos de dromadaire. Je suis PMR")
-    # state = State(last_user_message="Bonjour")
-    # state = State(last_user_message="J'aime le sport et la et la randonnée")
+    state = State(last_user_message="Bonjour, j'aime la randonnée")
     print(asyncio.run(graph.ainvoke(state)))
